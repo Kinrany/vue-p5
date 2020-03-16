3ae2f7bc090dcad7c579e982370c46b8620d092a
Kinrany <kinrany@yandex.com>
2020-03-17 00:43:32 +0300

    Fix Rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,6 @@
 import VuePlugin from "rollup-plugin-vue";
-import resolve from "rollup-plugin-node-resolve";
-import commonjs from "rollup-plugin-commonjs";
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
 
 export default {
   input: 'src/main.js',
