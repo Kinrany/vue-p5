078d542cfd4719a037dae8e467d4a3af3265723c
Kinrany <kinrany@yandex.com>
2020-03-17 01:47:20 +0300

    Fix "Vue is not defined"
    I think I remember fixing this bug before...

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import VueP5 from "./p5.vue";
 
 export default VueP5;
 
-if (Vue) {
+// `typeof Vue !== 'undefined'` prevents
+// the "Vue is not defined" error
+if (typeof Vue !== 'undefined' && Vue) {
   Vue.component('vue-p5', VueP5);
 }
