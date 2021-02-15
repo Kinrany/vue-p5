12f68357b68617d02dba749114ee3f8f225b2a03
Ruslan Fadeev <kinrany@yandex.com>
2021-02-15 22:30:43 +0300

    Do not register VueP5 globally

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,7 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 
 export default {
-  input: 'src/main.js',
+  input: 'src/vue-p5.js',
   output: {
     file: 'dist/vue-p5.js',
     format: 'umd',
--- src/main.js
+++
@@ -1,9 +0,0 @@
-import VueP5 from "./vue-p5";
-
-export default VueP5;
-
-// `typeof Vue !== 'undefined'` prevents
-// the "Vue is not defined" error
-if (typeof Vue !== 'undefined' && Vue) {
-  Vue.component('vue-p5', VueP5);
-}
